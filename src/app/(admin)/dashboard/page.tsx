5771d7837b55379bd9c08c06460e85a55d481f00
albert <aaalbertmo@gmail.com>
2024-04-16 16:25:19 +0800

    test useEffect

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function DashBoard() {
+  useEffect(() => {
+    for (let i = 0; i < 5; i++) {
+      console.log(`Effect ${i}`);
+    }
+  }, []); // Empty dependency array to run the effect only once
   return <div> this is a DashBoard Page</div>;
 }
