409edd79caaa03d614636dc5fd6b6b25a6259bfa
albert <aaalbertmo@gmail.com>
2024-10-07 15:24:20 +0800

    feat: add settings page

--- src/app/(admin)/settings/page.tsx
+++ src/app/(admin)/settings/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <div> this is a Setting Page</div>;
+import { Separator } from "@/components/ui/separator";
+import { ProfileForm } from "./profile-form";
+
+export default function SettingsPage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <ProfileForm />
+    </div>
+  );
 }
